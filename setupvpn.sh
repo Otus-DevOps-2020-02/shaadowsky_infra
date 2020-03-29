5e2ee8c6fdaca265759bb5685a5c77f48e4f03a8
Pavel Andreev <shaadowsky@gmail.com>
2020-03-29 15:34:05 +0800

    fixing pritunl script for ubuntu 18.04

--- setupvpn.sh
+++ setupvpn.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.4.list
-echo "deb http://repo.pritunl.com/stable/apt xenial main" > /etc/apt/sources.list.d/pritunl.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 0C49F3730359A14518585931BC711F9BA15703C6
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-apt-get --assume-yes update
-apt-get --assume-yes upgrade
-apt-get --assume-yes install pritunl mongodb-org
-systemctl start pritunl mongod
-systemctl enable pritunl mongod
+# for Ubuntu 18.04 LTS
+sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list << EOF
+deb https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse
+EOF
+
+sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+deb http://repo.pritunl.com/stable/apt bionic main
+EOF
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv E162F504A20CDF15827F718D4B7C549A058F8B6B
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+sudo apt-get update
+sudo apt-get --assume-yes install pritunl mongodb-server
+sudo systemctl enable --now pritunl mongodb
