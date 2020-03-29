b3273cc36bf7337928a2c7b026309166b4787218
Pavel Andreev <shaadowsky@gmail.com>
2020-03-29 15:34:58 +0800

    removing sudo from pritunl script

--- setupvpn.sh
+++ setupvpn.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # for Ubuntu 18.04 LTS
-sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list << EOF
+tee /etc/apt/sources.list.d/mongodb-org-4.2.list << EOF
 deb https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse
 EOF
 
-sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+tee /etc/apt/sources.list.d/pritunl.list << EOF
 deb http://repo.pritunl.com/stable/apt bionic main
 EOF
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv E162F504A20CDF15827F718D4B7C549A058F8B6B
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-sudo apt-get update
-sudo apt-get --assume-yes install pritunl mongodb-server
-sudo systemctl enable --now pritunl mongodb
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv E162F504A20CDF15827F718D4B7C549A058F8B6B
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+apt-get update
+apt-get --assume-yes install pritunl mongodb-server
+systemctl enable --now pritunl mongodb
