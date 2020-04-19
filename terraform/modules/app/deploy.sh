c8523c0029517e4542786cd5104d98d52fb48a9c
Pavel Andreev <shaadowsky@gmail.com>
2020-04-19 19:54:08 +0800

    expelled sudo from modules/app/deploy.sh

--- terraform/modules/app/deploy.sh
+++ terraform/modules/app/deploy.sh
@@ -7,6 +7,6 @@ git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
 cd $APP_DIR/reddit
 bundle install
 
-sudo mv /tmp/puma.service /etc/systemd/system/puma.service
-sudo systemctl start puma
-sudo systemctl enable puma
+mv /tmp/puma.service /etc/systemd/system/puma.service
+systemctl start puma
+systemctl enable puma
