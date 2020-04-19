d0e380a5a0c578f3333da2cc6db2085cdefbf90f
Pavel Andreev <shaadowsky@gmail.com>
2020-04-19 21:27:01 +0800

    returned sudo into terraform/modules/app/deploy.sh

--- terraform/modules/app/deploy.sh
+++ terraform/modules/app/deploy.sh
@@ -7,6 +7,6 @@ git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
 cd $APP_DIR/reddit
 bundle install
 
-mv /tmp/puma.service /etc/systemd/system/puma.service
-systemctl start puma
-systemctl enable puma
+sudo mv /tmp/puma.service /etc/systemd/system/puma.service
+sudo systemctl start puma
+sudo systemctl enable puma
