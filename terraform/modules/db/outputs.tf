a4742d40cf4d5399622399c4ae385abe9bac7163
Pavel Andreev <40653586+shaadowsky@users.noreply.github.com>
2020-05-01 07:33:51 +0800

    Ansible 1 (#10)
    * prepared for hw5
    
    * hw5 terraform-1 init
    
    * main part added
    
    * updated main.tf
    
    * added terraform.tfvars.example
    
    * independent workЭ
    
    * deprecating terraform version
    
    * HW* added
    
    * terraform fmt
    
    * hw** processing
    
    * added lb.tf
    
    * merged lb.tf into main.tf
    
    * transferred load-balacing into lb.tf
    
    * moved lb.tf into files/
    
    * forcing cleaning
    
    * added readme, partial exec of main HW
    
    * removing public_key variable
    
    * added app.json and db.json into packer dir
    
    * true added app.json and db.json into packer dir
    
    * added app.tf, db.tf and their disk variables
    
    * added packer jsons and tf files, modified readmes
    
    * added app and db modules into terraform
    
    * added all modules
    
    * truly added all modules
    
    * added prod and stage
    
    * cleanup before storage-bucket
    
    * checking main HW
    
    * added example files for stage and prod
    
    * added storage-backet.rf
    
    * designed hw*
    
    * updated readme
    
    * expelled sudo from modules/app/deploy.sh
    
    * returned sudo into terraform/modules/app/deploy.sh
    
    * end of daily work
    
    * corrected stage env: env = stage
    
    * added ansible inventory and requirements.txt
    
    * added variable machine_type
    
    * added inventory
    
    * added ansible.cfg
    
    * added host groups into inventory
    
    * added inventory.yml
    
    * added clone.yml
    
    * fetching
    
    * changing readme
    
    * Revert "changing readme"
    
    This reverts commit 56e842c723f168eb8db46868021385423db21265.
    
    * Revert "Merge branch 'master' into ansible-1"
    
    This reverts commit 2ab73d67507c756f596cc0d252bf496cd1db12b6, reversing
    changes made to d2f0889306b6dcf9665166c358c47f4b8c2a798d.

--- terraform/modules/db/outputs.tf
+++ terraform/modules/db/outputs.tf
@@ -1,3 +1,9 @@
+output "db_external_ip" {
+  description = "External ip address db"
+  value = google_compute_instance.db.network_interface[0].access_config[0].nat_ip
+}
+
 output "db_internal_ip" {
-  value = google_compute_instance.db.network_interface.0.network_ip
+  description = "Internal ip address db"
+  value       = google_compute_instance.db.network_interface[0].network_ip
 }
--- terraform/modules/db/variables.tf
+++ terraform/modules/db/variables.tf
@@ -13,6 +13,11 @@ variable zone {
   default     = "europe-north1-c"
 }
 
+variable machine_type {
+  description = "Machine type"
+  default = "f1-micro"
+}
+
 variable db_disk_image {
   description = "Disk image for reddit db"
   default     = "reddit-db-base"
